cddf78575d53975245e56017cc45ba4bc5e6bd2c
mikeadex <joko.adeleye@gmail.com>
2025-04-14 06:44:40 +0100

    fixing time out issue 2

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Start Gunicorn process with config file
-exec gunicorn ella_writer.wsgi:application -c /app/gunicorn.conf.py
+# Start Gunicorn process with environment variables for timeout
+exec gunicorn ella_writer.wsgi:application --bind 0.0.0.0:$PORT --timeout 120 --workers 3 --threads 3 --worker-class=gthread
